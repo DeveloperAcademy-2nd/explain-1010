a94d804e348400034e4d9d53c9123af5bb4f31a3
sunghun <leedool3003@gmail.com>
2023-09-21 11:05:29 +0900

    :tada: [init] init

---
+++ explain-1010/explain_1010App.swift
@@ -0,0 +1,17 @@
+//
+//  explain_1010App.swift
+//  explain-1010
+//
+//  Created by Kim SungHun on 2023/09/21.
+//
+
+import SwiftUI
+
+@main
+struct explain_1010App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
