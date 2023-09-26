f9c43fefb965e39609df0e5741845d50111592be
jieun <jieun_0330@naver.com>
2023-09-26 15:23:57 +0900

    :sparkles: [feat]뷰 수정 #6

--- explain-1010/ContentView.swift
+++ explain-1010/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("테스트 테스트 테스트")
+            Text("어디신디")
         }
         .padding()
     }
