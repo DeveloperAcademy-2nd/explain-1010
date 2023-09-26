c392c06a84fcb6ea540be5d3ce785f4fec0bb85a
sunghun <leedool3003@gmail.com>
2023-09-26 15:36:52 +0900

    :sparkles: [feat] 리뷰 후 수정 #9

--- explain-1010/ContentView.swift
+++ explain-1010/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("아지")
+            Text("오케이")
         }
         .padding()
     }
